210c8529ea93aaa7768242b6fbc6c5b023f5b02a
solomon-asmr <selemonasmr7@gmail.com>
2025-11-30 17:25:01 +0200

    fix: Final polish 2

--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, LogBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+
+// Ignore the specific warning globally, right at startup
+LogBox.ignoreLogs([
+  'new NativeEventEmitter',
+  '`new NativeEventEmitter()`', // Adding variations just to be safe
+]);
 
 AppRegistry.registerComponent(appName, () => App);
